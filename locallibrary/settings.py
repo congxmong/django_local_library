5f9067d33bd02565582fb9e52d154f3b82c05790
root <root@localhost.localdomain>
2019-01-11 00:55:31 +0100

    Update allowed_hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['secure-savannah-15752.herokuapp.com']
+ALLOWED_HOSTS = ['secure-savannah-15752.herokuapp.com', 'fathomless-ridge-97421.herokuapp.com']
 
 
 # Application definition
